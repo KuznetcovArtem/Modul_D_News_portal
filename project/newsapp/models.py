0294e4642d72d7955ebc5e089d41ea455e448718
KuznetcovArtem <109390032+KuznetcovArtem@users.noreply.github.com>
2022-09-21 15:46:37 +0300

    Update models.py
    category update name

--- project/newsapp/models.py
+++ project/newsapp/models.py
@@ -29,6 +29,9 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
