43e7fc017c4dac7f7ef5b2a1a62f2bca954c48cd
TEDD <toughguy1893@gmail.com>
2022-09-27 17:16:56 +0300

    final task D4 update

--- project/newsapp/models.py
+++ project/newsapp/models.py
@@ -4,7 +4,6 @@ from django.db.models import Sum
 from django.urls import reverse
 
 
-
 class Author(models.Model):
     authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
     ratingAuthor = models.SmallIntegerField(default=0)
@@ -30,6 +29,7 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.authorUser.username}'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
