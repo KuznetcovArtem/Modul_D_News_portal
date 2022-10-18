edb823af6d0eaf7aa59b33b8353c8527d74266b2
TEDD <toughguy1893@gmail.com>
2022-10-18 18:07:02 +0300

    update D6.2

--- project/newsapp/urls.py
+++ project/newsapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostCreate, PostEdit,PostDelete
+from .views import PostList, PostDetail, PostCreate, PostEdit, PostDelete, CategoryListView, subscribe
 
 urlpatterns = [
    path('', PostList.as_view(), name='post_list'),
@@ -7,4 +7,6 @@ urlpatterns = [
    path('create/', PostCreate.as_view(), name='post_create'),
    path('<int:pk>/edit/', PostEdit.as_view(), name='post_edit'),
    path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+   path('categories/<int:pk>', CategoryListView.as_view(), name='category_list'),
+   path('categories/<int:pk>/subscribe', subscribe, name='subscribe'),
 ]
--- project/newsapp/views.py
+++ project/newsapp/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Category
 from .filters import PostFilter
 from .forms import PostForm
 
@@ -50,3 +52,30 @@ class PostDelete(PermissionRequiredMixin, DeleteView):
     model = Post
     template_name = 'flatpages/post_delete.html'
     success_url = reverse_lazy('post_list')
+
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = 'flatpages/category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.postCategory = get_object_or_404(Category, id=self.kwargs['pk'])
+        queryset = Post.objects.filter(postCategory=self.postCategory).order_by('-dateCreation')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.postCategory.subscribers.all()
+        context['postCategory'] = self.postCategory
+        return context
+
+
+@login_required
+def subscribe(request, pk):
+    user = request.user
+    category = Category.objects.get(id=pk)
+    category.subscribers.add(user)
+
+    message = 'You have successfully subscribed to the newsletter of the category:'
+    return render(request, 'flatpages/subscribe.html', {'postCategory': category, 'message': message})
