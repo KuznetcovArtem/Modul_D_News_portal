b4310e0e8d96638547e61b7aa2a9492daee0f633
TEDD <toughguy1893@gmail.com>
2022-10-21 16:37:55 +0300

    d7.4

---
+++ project/app.py
@@ -0,0 +1,7 @@
+import redis
+
+red = redis.Redis(
+    host='redis-16944.c74.us-east-1-4.ec2.cloud.redislabs.com',
+    port=16944,
+    password='NGS1gVU9pjLecQtTALx9yZFco0N5iADm',
+)
--- project/project/settings.py
+++ project/project/settings.py
@@ -147,6 +147,7 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = "/news"
+LOGIN_URL = "/news"
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -164,13 +165,16 @@ ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
 
 SITE_URL = 'http://127.0.0.1:8000'
 
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = os.getenv('EMAIL_HOST_YANDEX')
+EMAIL_PORT = os.getenv('EMAIL_PORT_YANDEX')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER_YANDEX')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD_YANDEX')
 EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL_YANDEX')
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
+
+# if DEBUG:
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
